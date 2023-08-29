cea0272a263270a04a8dad24602d32f4ea9d1712
chip@shabazian.com <chip@shabazian.com>
2023-08-29 15:11:23 -0700

    Fixed check/create directory in setup_nvnc_podman.sh

--- setup_nvnc_podman.sh
+++ setup_nvnc_podman.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 [[ "$EUID" -ne 0 ]] && echo "Please run as root" && exit
-[[ -d /root/RHEL9-Workshop/config/ || mkdir -p /root/RHEL9-Workshop/config/
+[ ! -d "/root/RHEL9-Workshop/config/" ] && mkdir -p /root/RHEL9-Workshop/config/
 [[ -f /root/RHEL9-Workshop/config/gpte-studentpw.txt ]] || echo "student" > /root/RHEL9-Workshop/config/gpte-studentpw.txt
 [[ -f /root/RHEL9-Workshop/config/gpte-targethost.txt ]] || echo "node1.example.com" > /root/RHEL9-Workshop/config/gpte-targethost.txt
 ansible-galaxy collection install community.crypto ansible.posix community.general
