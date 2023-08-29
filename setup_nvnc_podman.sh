b08394bd96a148363ca9d986ad36ff686d4d675e
chip@shabazian.com <chip@shabazian.com>
2023-08-29 16:32:30 -0700

    Changed path for targethost /root/RHEL9-Workshop/config/gpte-pub-fqdn-node1.txt in  setup_nvnc_podman.sh and setup_workshop_novnc.yml

--- setup_nvnc_podman.sh
+++ setup_nvnc_podman.sh
@@ -2,6 +2,7 @@
 [[ "$EUID" -ne 0 ]] && echo "Please run as root" && exit
 [ ! -d "/root/RHEL9-Workshop/config/" ] && mkdir -p /root/RHEL9-Workshop/config/
 [[ -f /root/RHEL9-Workshop/config/gpte-studentpw.txt ]] || echo "student" > /root/RHEL9-Workshop/config/gpte-studentpw.txt
-[[ -f /root/RHEL9-Workshop/config/gpte-targethost.txt ]] || echo "node1.example.com" > /root/RHEL9-Workshop/config/gpte-targethost.txt
+#[[ -f /root/RHEL9-Workshop/config/gpte-targethost.txt ]] || echo "node1.example.com" > /root/RHEL9-Workshop/config/gpte-targethost.txt
+[[ -f /root/RHEL9-Workshop/config/gpte-pub-fqdn-node1.txt ]] || echo "node1.example.com" > /root/RHEL9-Workshop/config/gpte-pub-fqdn-node1.txt
 ansible-galaxy collection install community.crypto ansible.posix community.general
 ansible-playbook ./ansible/setup_workshop_novnc.yml -i node1.example.com,
