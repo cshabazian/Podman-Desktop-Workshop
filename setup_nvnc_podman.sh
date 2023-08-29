ef19fe7904dbcc02589be114d33bf1679899900e
Chip Shabazian <chip@shabazian.com>
2023-08-29 14:53:01 -0700

    Added script and playbook to install novnc and podman desktop

---
+++ setup_nvnc_podman.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+[[ "$EUID" -ne 0 ]] && echo "Please run as root" && exit
+[[ -f /root/RHEL9-Workshop/config/gpte-studentpw.txt ]] || echo "student" > /root/RHEL9-Workshop/config/gpte-studentpw.txt
+[[ -f /root/RHEL9-Workshop/config/gpte-targethost.txt ]] || echo "node1.example.com" > /root/RHEL9-Workshop/config/gpte-targethost.txt
+ansible-galaxy collection install community.crypto ansible.posix community.general
+ansible-playbook ./ansible/setup_workshop_novnc.yml -i node1.example.com,
