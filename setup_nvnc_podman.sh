37e25522cab5328dfd5335f5ea05fffb787e6850
chip@shabazian.com <chip@shabazian.com>
2023-08-30 09:57:29 -0700

    fixed pwd

--- setup_nvnc_podman.sh
+++ setup_nvnc_podman.sh
@@ -2,7 +2,6 @@
 [[ "$EUID" -ne 0 ]] && echo "Please run as root" && exit
 [ ! -d "/root/RHEL9-Workshop/config/" ] && mkdir -p /root/RHEL9-Workshop/config/
 [[ -f /root/RHEL9-Workshop/config/gpte-studentpw.txt ]] || echo "student" > /root/RHEL9-Workshop/config/gpte-studentpw.txt
-#[[ -f /root/RHEL9-Workshop/config/gpte-targethost.txt ]] || echo "node1.example.com" > /root/RHEL9-Workshop/config/gpte-targethost.txt
 [[ -f /root/RHEL9-Workshop/config/gpte-pub-fqdn-node1.txt ]] || echo "node1.example.com" > /root/RHEL9-Workshop/config/gpte-pub-fqdn-node1.txt
 ansible-galaxy collection install community.crypto ansible.posix community.general
-ansible-playbook $(PWD)ansible/setup_workshop_novnc.yml -i node1,
+ansible-playbook $(pwd)/ansible/setup_workshop_novnc.yml -i node1,
